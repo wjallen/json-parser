b4d4ce011316e0da7fb1926ecf2b33190162cf48
Joe Allen <wallen@tacc.utexas.edu>
2021-02-02 11:52:21 -0600

    added a docstring to a function

--- json_ex.py
+++ json_ex.py
@@ -1,6 +1,10 @@
 import json
 
 def check_char_count(mystr):
+    """ 
+        This is a docstring - it is a descriptive message about what
+        this function is and how to use it
+    """
 
     assert isinstance(mystr, str), 'Input to this function should be a string'
 
